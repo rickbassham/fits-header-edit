896b45f7bdbcd9c73c2e88ced4ea33d423eb864a
Rick Bassham <brodrick.bassham@gmail.com>
2021-09-11 10:35:40 -0400

    feat: initial commit; ability to batch edit fits headers from the command line

---
+++ main.py
@@ -0,0 +1,33 @@
+import argparse
+import glob
+from typing import Any
+
+from astropy.io import fits
+
+
+def edit_fits_headers(
+    path: str, header: str, value: Any, recursive: bool = False
+) -> None:
+    for file_path in glob.iglob(path, recursive=recursive):
+        fits.setval(
+            file_path,
+            header=header,
+            value=value,
+        )
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Set FITS file headers.")
+    parser.add_argument("-p", "--path", help="path to the FITS file(s)", required=True)
+    parser.add_argument("--header", help="header name to set", required=True)
+    parser.add_argument("--value", help="value to set", required=True)
+    parser.add_argument(
+        "-r",
+        "--recursive",
+        help="recursive search",
+        default=False,
+        action="store_true",
+    )
+    args = parser.parse_args()
+
+    edit_fits_headers(args.path, args.header, args.value, args.recursive)
